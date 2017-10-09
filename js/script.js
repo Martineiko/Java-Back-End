7e6e8a66c8d5bce59e98d7e88b997b46692e02ca
Martineiko <palox.medina@gmail.com>
2017-10-09 18:45:38 -0500

    Zozor v.02
    Puro bootstrap mi compa

---
+++ js/script.js
@@ -0,0 +1,72 @@
+var un_numero = 1;
+var un_flotante = 1.5;
+
+var otro_numero='1';
+
+var un_texto='esto es un texto';
+console.log(typeof(un_texto));
+var esVerdad = true;
+
+console.log(un_numero);
+console.log(un_flotante);
+console.log(otro_numero);
+console.log(un_texto);
+console.log(esVerdad);
+
+ /* comentario multilinea
+ //comentario simple
+ var x; //declara una variable
+ x = 3+y;//asigna valor a la variable x
+ foo(x,y);//llama a la funcion foo con parametros 'x''e''y'
+ obj.bar(3);//llama al metodo 'bar' del object 'obj'
+ //declaracion condicional
+ if (x===0){//x es igual a cero?
+ 	x=123;
+ }
+
+ //define la funcion 'baz' con parametros 'a' y 'b'
+ function baz(a,b){
+ 	return a+b;
+ }
+
+var x = "0";
+
+if (x == 0) {
+	console.log("Somos iguales");
+} else {
+	console.log("No somos iguales");
+}
+
+//var x = 0;
+
+if (x === 0) {
+	console.log("Somos iguales y del mismo tipo");
+} else {
+	console.log("No somos tan iguales");
+}
+*/
+//prubas de 
+/*var a = null;
+var b = " Hola";
+var c = ' y adiós';
+
+var d = 1;
+
+var e = Math.PI;
+
+var f = 'Math.PI';
+
+
+if(e == f)
+{
+	alert("'e' igual a 'f'");
+}
+
+if( d===a)
+{
+	alert(a)
+}
+else
+{
+	alert(a + b + c +d+" : " +e+" \n -" +f+"-"+typeof(e));
+}*/
